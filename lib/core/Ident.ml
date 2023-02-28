dd2576c4f8ab9d7feed25a60978853144529583d
solomon <ssbothwell@gmail.com>
2023-02-27 20:01:58 -0800

    defn: adds name equality function to Ident.ml

--- lib/core/Ident.ml
+++ lib/core/Ident.ml
@@ -9,6 +9,13 @@ let user parts = `User parts
 (** Machine generated names *)
 let machine nm = `Machine nm
 
+let equal n1 n2 =
+  match (n1, n2) with
+  | `Anon, `Anon -> true
+  | `User path1, `User path2 -> List.equal String.equal path1 path2 
+  | `Machine i1, `Machine i2 -> Int.equal i1 i2
+  | _ -> false
+
 let pp_path fmt path =
   Format.pp_print_list
     ~pp_sep:(fun fmt () -> Format.pp_print_string fmt ".")
--- lib/core/Ident.mli
+++ lib/core/Ident.mli
@@ -5,5 +5,7 @@ val anon : t
 val user : path -> t
 val machine : int -> t
 
+val equal : t -> t -> bool
+
 val pp_path : Format.formatter -> path -> unit
 val pp : Format.formatter -> t -> unit
