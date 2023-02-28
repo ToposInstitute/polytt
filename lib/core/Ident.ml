1c0a0a9335e64c2cd9df8da341b04577416d8597
solomon <ssbothwell@gmail.com>
2023-02-27 20:01:32 -0800

    doc: add descriptions for name identiifer types

--- lib/core/Ident.ml
+++ lib/core/Ident.ml
@@ -1,8 +1,12 @@
 type path = Yuujinchou.Trie.path
+(** The type of identifiers *) 
 type t = [ `Anon | `User of path | `Machine of int ]
 
+(** Anonymous names, eg., underscores *)
 let anon = `Anon
+(** User defined names, potentially qualified *)
 let user parts = `User parts
+(** Machine generated names *)
 let machine nm = `Machine nm
 
 let pp_path fmt path =
