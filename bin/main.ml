0cc43d09467608d45ba4119f44d279781ffe928a
Owen Lynch <root@owenlynch.org>
2023-02-28 12:26:15 -0800

    enhance: now takes in a command line argument for what to load

--- bin/main.ml
+++ bin/main.ml
@@ -1,1 +1,12 @@
-let () = Loader.load "./examples/prelude.poly"
+open Cmdliner
+
+let file =
+  let doc = "Load and evaluate $(FILE)." in
+  let docv = "FILE" in
+  Arg.(required & pos 0 (some string) None & info [] ~docv ~doc)
+
+let load_t = Term.(const Loader.load $ file)
+
+let cmd = Cmd.v (Cmd.info "polytt") load_t
+
+let () = exit (Cmd.eval cmd)
