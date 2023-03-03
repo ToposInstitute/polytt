395ad9d0f8fde37e22dbebde0b207ee01e72534b
solomon <ssbothwell@gmail.com>
2023-03-02 16:02:24 -0800

    bug: fix failing pp test

--- test/polytt.ml
+++ test/polytt.ml
@@ -19,7 +19,7 @@ let () = test "λ x → x x (x x)" (S.Lam (abs "x", S.Ap (S.Ap (S.Var 0, S.Var 0
 (* BlockArguments is an extension :-/ *)
 let () = test "λ x → x (λ y → x)" (S.Lam (abs "x", S.Ap (S.Var 0, S.Lam (abs "y", S.Var 1))))
 let () = test "λ x → x (fst x x)" (S.Lam (abs "x", S.Ap (S.Var 0, S.Ap (S.Fst (S.Var 0), S.Var 0))))
-let () = test "(x : type) → λ x → x" (S.Pi (abs "x", S.Univ, S.Lam (abs "x", S.Var 0)))
+let () = test "(x : Type) → λ x → x" (S.Pi (abs "x", S.Univ, S.Lam (abs "x", S.Var 0)))
 let () = test "(x : λ y → y) × x" (S.Sigma (abs "x", S.Lam (abs "y", S.Var 0), S.Var 0))
 let () = test "λ x → succ (succ x)" (S.Lam (abs "x", S.Succ (S.Succ (S.Var 0))))
 let () = test "λ x → 2" (S.Lam (abs "x", S.Succ (S.Succ S.Zero)))
