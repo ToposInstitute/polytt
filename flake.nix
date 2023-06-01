9e7663f1716c4046d0643ba21ba4d896047352d1
solomon <ssbothwell@gmail.com>
2023-06-01 12:40:10 -0700

    Adds Nix Build

--- flake.nix
+++ flake.nix
@@ -1,24 +1,39 @@
 {
-  description = "A programming language for Polynomial Functors";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-22.11";
+    opam-nix.url = "github:tweag/opam-nix";
     flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.follows = "opam-nix/nixpkgs";
   };
-
-  outputs = { self, flake-utils, nixpkgs }:
+  outputs = { self, flake-utils, opam-nix, nixpkgs }@inputs:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = import nixpkgs {
-          inherit system;
+        pkgs = nixpkgs.legacyPackages.${system};
+        on = opam-nix.lib.${system};
+        localPackagesQuery = builtins.mapAttrs (_: pkgs.lib.last)
+          (on.listRepo (on.makeOpamRepo ./.));
+        devPackagesQuery = {
+          ocaml-lsp-server = "*";
+          ocp-indent = "*";
         };
+        query = devPackagesQuery // {
+          ocaml-base-compiler = "*";
+        };
+        scope = on.buildDuneProject { } "polytt" ./. query;
+        devPackages = builtins.attrValues
+          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope);
+        packages =
+          pkgs.lib.getAttrs (builtins.attrNames localPackagesQuery) scope;
       in
       {
-        devShell = pkgs.mkShell {
-          nativeBuildInputs = [
+        legacyPackages = scope;
+
+        packages = packages // { default = packages.polytt; };
+
+        devShells.default = pkgs.mkShell {
+          inputsFrom = builtins.attrValues packages;
+          buildInputs = devPackages ++ [
             pkgs.fd
             pkgs.nixpkgs-fmt
-            pkgs.opam
             pkgs.pkg-config
             pkgs.shellcheck
           ];
