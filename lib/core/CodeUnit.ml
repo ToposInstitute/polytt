b89ca3467c23e68d0f2ca3a3d2a11c1a10b26efc
Reed Mullanix <reedmullanix@gmail.com>
2023-06-23 10:26:40 -0700

    def: update code units to use effects

--- lib/core/CodeUnit.ml
+++ lib/core/CodeUnit.ml
@@ -1,15 +1,33 @@
-type def = { tp : Syntax.t; def : Syntax.t }
+type def = { tp : Domain.t; value : Domain.t }
+type code_unit = (def, CCVector.rw) CCVector.t
 
-type t = { id : int; defs : (def, CCVector.rw) CCVector.t }
+type env = { current: int; code_units : (code_unit, CCVector.rw) CCVector.t }
 
-let empty id = { id; defs = CCVector.create () }
+module Eff = Algaeff.Reader.Make (struct type nonrec env = env end)
 
-let add_def nm def code_unit =
-  let lvl = CCVector.length code_unit.defs in
-  let glbl = { Global.nm; lvl; code_unit = code_unit.id } in
-  CCVector.push code_unit.defs def;
+let with_new_unit k =
+  let env = Eff.read () in
+  let id = CCVector.length env.code_units in
+  let code_unit = CCVector.create () in
+  CCVector.push env.code_units code_unit;
+  Eff.scope (fun env -> { env with current = id }) k
+
+let current_code_unit () =
+  let env = Eff.read () in
+  CCVector.get env.code_units env.current, env.current
+
+let add_def nm def =
+  let code_unit, id = current_code_unit () in
+  let lvl = CCVector.length code_unit in
+  let glbl = { Global.nm; lvl; code_unit = id } in
+  CCVector.push code_unit def;
   glbl
 
-let get_def (glbl : Global.t) code_unit =
-  assert (glbl.code_unit = code_unit.id);
-  CCVector.get code_unit.defs glbl.lvl 
+let get_def (glbl : Global.t) =
+  let env = Eff.read () in
+  let code_unit = CCVector.get env.code_units glbl.code_unit in
+  CCVector.get code_unit glbl.lvl
+
+let run k =
+  let code_units = CCVector.create () in
+  Eff.run ~env:{ current = 0; code_units } k
--- lib/core/CodeUnit.mli
+++ lib/core/CodeUnit.mli
@@ -1,14 +1,16 @@
-type t
-
 (** A single definition stored in a code-unit. *)
-type def = { tp : Syntax.t; def : Syntax.t }
+type def = { tp : Domain.t; value : Domain.t }
 
-(** Create a new code-unit using the provided ID. *)
-val empty : int -> t
+(** Create a new code-unit, and call the provided function
+    with that code-unit as the current code-unit. *)
+val with_new_unit : (unit -> 'a) -> 'a
 
-(** Add a definition to a code-unit.
+(** Add a definition to the current code-unit.
     This returns a {!type:Global.t} associated to the new definition. *)
-val add_def : Ident.t -> def -> t -> Global.t
+val add_def : Ident.t -> def -> Global.t
 
-(** Look up a definition in a code-unit. *)
-val get_def : Global.t -> t -> def
+(** Look up a definition. *)
+val get_def : Global.t -> def
+
+(** Handle the code-unit effects, starting with no code-units defined. *)
+val run : (unit -> 'a) -> 'a
