da46df74f5a4ddc96348a4361a2747a643ecbdaf
solomon <ssbothwell@gmail.com>
2023-07-08 23:41:17 -0700

    Updates flake.nix

--- flake.nix
+++ flake.nix
@@ -9,24 +9,42 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
         on = opam-nix.lib.${system};
+
         localPackagesQuery = builtins.mapAttrs (_: pkgs.lib.last)
           (on.listRepo (on.makeOpamRepo ./.));
+
         devPackagesQuery = {
           ocaml-lsp-server = "*";
           ocp-indent = "*";
           merlin = "*";
         };
+
         query = devPackagesQuery // {
           ocaml-base-compiler = "*";
         };
-        scope = on.buildDuneProject { } "polytt" ./. query;
+
+        scope = on.buildDuneProject { pkgs = pkgs.pkgsStatic; } "polytt" ./. query;
+
+        overlay = self: super: {
+          # Prevent unnecessary dependencies on the resulting derivation
+          polytt = super.polytt.overrideAttrs (_: {
+            removeOcamlReferences = true;
+            postFixup = "rm -rf $out/nix-support";
+          });
+        };
+
+        scope' = scope.overrideScope' overlay;
+
+        # Packages from devPackagesQuery
         devPackages = builtins.attrValues
-          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope);
+          (pkgs.lib.getAttrs (builtins.attrNames devPackagesQuery) scope');
+
+        # Packages in this workspace
         packages =
-          pkgs.lib.getAttrs (builtins.attrNames localPackagesQuery) scope;
+          pkgs.lib.getAttrs (builtins.attrNames localPackagesQuery) scope';
       in
       {
-        legacyPackages = scope;
+        legacyPackages = scope';
 
         packages = packages // { default = packages.polytt; };
 
